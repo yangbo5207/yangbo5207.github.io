9772d4f2abe4939b2f4d3246467babb2af35b83b
yangbo <yangbo@tigerbrokers.com>
2016-03-01 17:58:28 +0800

    添加主页延迟加载动画

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 (function($, undefined) {
 
+    LazyLoad($('.item'), function() {
+        var aninodes = $(this).addClass('animated').find('[data-animation]');
+        aninodes.each(function() {
+            $(this).addClass($(this).attr('data-animation'));
+        });
+    });
 
 })(jQuery);
